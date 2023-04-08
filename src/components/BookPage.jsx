6323119f761cc652c56702450d5b2a6d25a5f601
{penguin311} <{lst5561@naver.com}>
2023-04-08 14:21:36 +0900

    test221

--- src/components/BookPage.jsx
+++ src/components/BookPage.jsx
@@ -1,79 +1,93 @@
-import React, { useEffect, useState } from 'react'
-import {Row, Col, Button, Form} from 'react-bootstrap'
-import axios from 'axios'
-import Book from './Book';
+import React, { useEffect, useRef, useState } from "react";
+import { Row, Col, Button, Form } from "react-bootstrap";
+import axios from "axios";
 
 const BookPage = () => {
-    const [loading, setLoading] = useState(false);
-    const [books, setBooks] = useState([]);
-    const [page, setPage] = useState(1);
-    const [total, setTotal] = useState(0);
-    const [is_end, setIs_end] = useState(false);
-    const [query, setQuery] = useState('리액트');
+  const [loading, setLoading] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [is_end, setIs_end] = useState(false);
+  const [query, setQuery] = useState("리액트");
+  const ref_query = useRef(null);
 
-    const getBooks = async() => {
-        const url = "https://dapi.kakao.com/v3/search/book?target=title";
-        const config = {
-            headers: {"Authorization": "KakaoAK 4361e376f3dd2e9b4ec4f09da086421b"},
-            params: {"query": query, "size": 6, "page": page}
-        }
-        setLoading(true);
-        const result = await axios(url, config);
-        console.log(result.data);
-        const data=result.data.documents.map(book=>{
-            const fmtPrice=book.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-            return { ...book, fmtPrice:fmtPrice, show:false }
-        })
-        setBooks(data);
-        setTotal(result.data.meta.pageable_count);
-        setIs_end(result.data.meta.is_end);
-        setLoading(false);
-    }
+  const onSummit = (e) => {
+    e.preventDefault();
+    getBooks();
+  };
 
-    useEffect(()=>{
-        getBooks();
-    },[page]);
+  const getBooks = async () => {
+    const url = "https://dapi.kakao.com/v3/search/book?target=title";
+    const config = {
+      headers: { Authorization: "KakaoAK 4361e376f3dd2e9b4ec4f09da086421b" },
+      params: { query: query, size: 6, page: page },
+    };
 
-    const onSearch = () => {
-        setPage(1);
-        getBooks();
-    }
+    setLoading(true);
+    const result = await axios(url, config);
 
-    if(loading) return <h1 className='text-center my-5'>로딩중......</h1>
-    return (
-        <Row className='justify-content-center mx-2 my-5'>
-            <h1 className='text-center mb-5'>도서검색</h1>
-            <Row className='my-2'>
-                <Col xs={6} md={2}>
-                    <Form onSubmit={ onSearch }>
-                        <Form.Control value={query} placeholder='검색어'
-                            onChange={(e)=>setQuery(e.target.value)}/>
-                    </Form>
-                </Col>
-                <Col>검색수: {total}건</Col>
-            </Row>
-            <hr/>
-            <Row className='my-2 justify-content-center'>
-                {books.map(book=>
-                    <Col key={book.isbn} className='m-2 box'>
-                        <div><img src={!book.thumbnail ? 'http://via.placeholder.com/120x170':book.thumbnail}/></div>
-                        <div className="ellipsis">{book.title}</div>
-                        <div>{book.fmtPrice}원</div>
-                        <div class="ellipsis">{book.authors}</div>
-                        <Book book={book}/>
-                    </Col>
-                )}
-            </Row>
-            <div className='text-center my-3'>
-                <Button disabled={page === 1 && true}
-                    onClick={()=>setPage(page-1)}>이전</Button>
-                <span className='mx-2'>{page}</span>
-                <Button disabled={is_end}
-                    onClick={()=>setPage(page+1)}>다음</Button>
-            </div>
+    setTotal(result.data.meta.pageable_count);
+    setIs_end(result.data.meta.is_end);
+    setBooks(result.data.documents);
+    console.log(result);
+    setLoading(false);
+    ref_query.current.focus();
+  };
+
+  useEffect(() => {
+    getBooks();
+  }, [page]);
+
+  if (loading) return <h1 className="text-center my-5">로딩중임 ...</h1>;
+  return (
+    <Row className="my-5 mx-2">
+      <Row>
+        <Col>
+          <Form onSubmit={onSummit}>
+            <Form.Control
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+              placeholder="검색어"
+              ref={ref_query}
+            />
+          </Form>
+        </Col>
+        <Col>검색수 : {total}</Col>
+      </Row>
+      <Col>
+        <h1 className="text-center">도서검색</h1>
+        <Row>
+          {books.map((book) => (
+            <Col key={book.isbn} className="box my-2">
+              <div>
+                <img
+                  src={
+                    !book.thumbnail
+                      ? "http://via.placeholder.com/120x170"
+                      : book.thumbnail
+                  }
+                />
+              </div>
+              <div className="eilipsis">{book.title}</div>
+              <div className="eilipsis">{book.price}원</div>
+            </Col>
+          ))}
         </Row>
-    )
-}
+      </Col>
+      <div>
+        <Button disabled={page == 1 && true} onClick={() => setPage(page - 1)}>
+          이전
+        </Button>
+        <span className="mx-3">{page}</span>
+        <Button
+          disabled={is_end == 1 && true}
+          onClick={() => setPage(page + 1)}
+        >
+          다음
+        </Button>
+      </div>
+    </Row>
+  );
+};
 
-export default BookPage
-
+export default BookPage;
